2e8512be344b0603a09051906cde93fc2089daa4
c0demic <note.revo@gmail.com>
2017-04-07 21:40:54 -0300

    successfully make react router run and the react-router-dom can do the job which I thought react-route can do

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,10 +4,11 @@ import HomePage from './components/HomePage.jsx';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {createBrowserHistory, router} from 'react-router';
-
+import BasicExample from './containers/test.js';
 ReactDom.render(
         (<MuiThemeProvider muiTheme={getMuiTheme()}>
-        <HomePage />
+        {/*<HomePage />*/}
+        <BasicExample />
         </MuiThemeProvider>),
         document.getElementById('react-app')
 );
